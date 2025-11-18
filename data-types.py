e079be50d76982104d0b32fa9b56ab9d6567e04f
Gilles <Gilles@DESKTOP>
2025-11-18 17:25:34 +0100

    play around with basic data types, for easy reference for myself

---
+++ data-types.py
@@ -0,0 +1,17 @@
+iamString = "boop"
+iamInt = 9
+iamFloating = 20.5
+iamComplex = 1j
+
+iamList = ["beep", "boop"]
+iamTuple = ("beep", "boop")
+iamDictionary = {"name": "jane", "age": 21}
+iamSet = {"one", "two", "three"}
+
+iamBool = True
+
+iamCastToInt = int(2.8)  # will be 2
+iamCastToString = str(2.9)
+
+iWillBeTrue = "free" in "all beer is free on weekends"
+iWillAlsoBeTrue = "wednesday" not in "all beer is free on weekends"
---
+++ strings.py
@@ -0,0 +1,7 @@
+firstString = "Hello world"
+
+print(firstString.upper())
+
+print("spaces are removed:", "  Hello world  ".strip())
+
+print(firstString.replace("Hello", "Goodbye"))
